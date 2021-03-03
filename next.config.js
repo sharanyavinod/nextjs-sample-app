8ec38085503ff716b2c5ababcd407e61a6b130d8
sharanyavinod <sharanyavinod90@gmail.com>
2021-03-03 17:00:33 +0100

    Updated next.config for manifest

--- next.config.js
+++ next.config.js
@@ -2,7 +2,20 @@ const withAssetsManifest = require('next-assets-manifest');
 
 module.exports = withAssetsManifest({
   assetsManifest: {
-    // https://github.com/webdeveric/webpack-assets-manifest#options-read-the-schema
-    output: 'assets-manifest.json',
-  },
+    output: "../public/asset-manifest.json",
+    publicPath: true,
+    transform: (assets, manifest) => {
+        const entrypoints = [];
+      	for(let file in assets) {
+        	entrypoints.push(assets[file]);
+        }
+        return {
+          files: assets,
+          entrypoints: entrypoints
+        };
+      },
+       done(manifest) {
+          console.log( `${manifest}` );
+      }
+  }
 });
