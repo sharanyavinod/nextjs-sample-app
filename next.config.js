1ed8a8591efd289eda13ab8ec080ea2381873b33
sharanyainod <sharanyavinod90@gmail.com>
2021-03-05 11:17:31 +0100

    SPA SDK added

--- next.config.js
+++ next.config.js
@@ -1,21 +1,33 @@
 const withAssetsManifest = require('next-assets-manifest');
 
 module.exports = withAssetsManifest({
+  //  async rewrites() {
+  //   return [
+  //     // we need to define a no-op rewrite to trigger checking
+  //     // all pages/static files before we attempt proxying
+  //     {
+  //       source: '/:path*',
+  //       destination: '/:path*'
+  //     },
+  //     {
+  //       source: '/:path*',
+  //       destination: `http://localhost:4502/:path*`
+  //     }
+  //   ]
+  // },
   assetsManifest: {
     output: "../public/asset-manifest.json",
-    publicPath: true,
     transform: (assets, manifest) => {
         const entrypoints = [];
       	for(let file in assets) {
-        	entrypoints.push(assets[file]);
+            if(assets[file].endsWith(".js")) {
+            	entrypoints.push(assets[file]);
+            }
         }
         return {
           files: assets,
           entrypoints: entrypoints
         };
-      },
-       done(manifest) {
-          console.log( `${manifest}` );
       }
   }
 });
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,10 @@
-import '../styles/global.css'
+import { ModelManager } from "@adobe/aem-spa-page-model-manager";
+import CustomModelClient from '../lib/CustomModelClient';
+
+const modelClient = new CustomModelClient('http://localhost:4502');
+ModelManager.initializeAsync({
+    modelClient
+});
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
